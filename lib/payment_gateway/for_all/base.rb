48570053ebb269eefd49b000528cc833a718340f
Jhonatan Teixeira <johnn_lds@hotmail.com>
2018-10-05 10:28:40 -0300

    implement base request method that will be inherited from other classes

--- lib/payment_gateway/for_all/base.rb
+++ lib/payment_gateway/for_all/base.rb
@@ -1,12 +1,32 @@
 module PaymentGateway
   module ForAll
     class Base
-      API_URL = ''
+      class PublicKeyNotFound < StandardError; end
+      class GatewayError      < StandardError; end
+
+      API_URL = 'https://gateway.homolog-interna.4all.com'
 
       def initialize
-        public_key = PaymentGateway::ForAll.configuration.public_key
+        @public_key = PaymentGateway::ForAll.configuration.public_key
+        raise PublicKeyNotFound if @public_key.blank?
       end
 
+      protected
+
+      def request(method, url, options={ headers: {}, body: {}, params: {} })
+        headers = { 'Content-Type' => 'application/json' }
+        headers.merge!(options[:headers]) unless options[:headers].nil?
+        options[:body].merge({'publicApiKey': @public_key})
+        response = HTTP.
+            headers('Content-Type' => 'application/json').
+            send(method, url, json: options[:body])
+        if response.status.eql?(200)
+          response.parse.with_indifferent_access
+        else
+          error_message = "#{response.status} - #{response.parse['errors'] || response.parse['message']}"
+          raise GatewayError, error_message
+        end
+      end
     end
   end
 end
