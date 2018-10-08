63771dd4d7abf0cf05b8a21070f2d822a3cd09d0
Jhonatan Teixeira <johnn_lds@hotmail.com>
2018-10-08 16:23:03 -0300

    add some dependencies to payment_gateway

--- lib/payment_gateway.rb
+++ lib/payment_gateway.rb
@@ -1,5 +1,8 @@
 require 'active_support/all'
 require 'payment_gateway/for_all'
 require 'payment_gateway/for_all/base'
+require 'payment_gateway/for_all/vault'
+require 'payment_gateway/for_all/card'
+require 'payment_gateway/for_all/transaction'
 
 #ao iniciar o IRB precisa dar o require!!
