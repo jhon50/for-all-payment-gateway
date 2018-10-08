ac06277776ca72dce34fbed4f0b191954200bbba
Jhonatan Teixeira <johnn_lds@hotmail.com>
2018-10-08 16:24:37 -0300

    base config for local tests purpose

--- lib/payment_gateway/for_all/base.rb
+++ lib/payment_gateway/for_all/base.rb
@@ -1,3 +1,5 @@
+require 'http'
+require 'pry-rails'
 module PaymentGateway
   module ForAll
     class Base
@@ -8,24 +10,33 @@ module PaymentGateway
 
       def initialize
         @public_key = PaymentGateway::ForAll.configuration.public_key
-        raise PublicKeyNotFound if @public_key.blank?
+        # remove this to assure public key is present!
+        # raise PublicKeyNotFound if @public_key.blank?
       end
 
       protected
 
-      def request(method, url, options={ headers: {}, body: {}, params: {} })
-        headers = { 'Content-Type' => 'application/json' }
-        headers.merge!(options[:headers]) unless options[:headers].nil?
-        options[:body].merge({'publicApiKey': @public_key})
+      def request(method, url, options={ body: {}, params: {} })
+        options[:body].merge!({'publicApiKey': 'zNkquQz38RbzzTl/GmzyZ5LO3/BBw8q97WlMMEzZBwo='})
         response = HTTP.
             headers('Content-Type' => 'application/json').
-            send(method, url, json: options[:body])
+            send(method, url, json: options[:body].with_indifferent_access)
         if response.status.eql?(200)
           response.parse.with_indifferent_access
         else
           error_message = "#{response.status} - #{response.parse['errors'] || response.parse['message']}"
           raise GatewayError, error_message
         end
+      end
+
+      def access_key
+        access_key = PaymentGateway::ForAll.configuration.access_key
+        access_key.blank? ? Vault.new.request_key : access_key
+      end
+
+      def merchant_key
+        # PaymentGateway::ForAll.configuration.merchant_key
+        'C83zDb3wit+edGFF7jBWg6eN9R8S6Klgwl5ZN+BlNZw='
       end
     end
   end
