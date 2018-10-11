6b84b86959e79e11e0d9b58f1de416f892afb4b1
Jhonatan Teixeira <johnn_lds@hotmail.com>
2018-10-11 11:06:12 -0300

    use merchantKey instead of publicKey

--- lib/payment_gateway/for_all/base.rb
+++ lib/payment_gateway/for_all/base.rb
@@ -3,21 +3,20 @@ require 'pry-rails'
 module PaymentGateway
   module ForAll
     class Base
-      class PublicKeyNotFound < StandardError; end
+      class MerchantKeyNotFound < StandardError; end
       class GatewayError      < StandardError; end
 
       API_URL = 'https://gateway.homolog-interna.4all.com'
 
       def initialize
-        @public_key = PaymentGateway::ForAll.configuration.public_key
-        # remove this to assure public key is present!
-        # raise PublicKeyNotFound if @public_key.blank?
+        @merchant_key = PaymentGateway::ForAll.configuration.merchant_key
+        raise MerchantKeyNotFound if @merchant_key.blank?
       end
 
       protected
 
       def request(method, url, options={ body: {}, params: {} })
-        options[:body].merge!({'publicApiKey': 'zNkquQz38RbzzTl/GmzyZ5LO3/BBw8q97WlMMEzZBwo='})
+        options[:body].merge!({'merchantKey': @merchant_key})
         response = HTTP.
             headers('Content-Type' => 'application/json').
             send(method, url, json: options[:body].with_indifferent_access)
@@ -32,11 +31,6 @@ module PaymentGateway
       def access_key
         access_key = PaymentGateway::ForAll.configuration.access_key
         access_key.blank? ? Vault.new.request_key : access_key
-      end
-
-      def merchant_key
-        # PaymentGateway::ForAll.configuration.merchant_key
-        'C83zDb3wit+edGFF7jBWg6eN9R8S6Klgwl5ZN+BlNZw='
       end
     end
   end
