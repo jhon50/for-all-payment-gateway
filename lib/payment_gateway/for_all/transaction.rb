f3abea8e5a6eaeef39b44f6115bb6db55667e2a5
Jhonatan Teixeira <johnn_lds@hotmail.com>
2018-10-11 11:06:42 -0300

    add documentation and post body samples

--- lib/payment_gateway/for_all/transaction.rb
+++ lib/payment_gateway/for_all/transaction.rb
@@ -1,67 +1,67 @@
 module PaymentGateway
   module ForAll
     class Transaction < Base
-      attr_accessor :transaction_id
 
-      # You can pass card_token or card_nonce to this method
-      # card_nonce expires in 30 minutes, but the token is permanent.
-      def create(card_token)
-        self.transaction_id = request(:post, endpoint, body: build_transaction_body(card_token))
+      # POST
+      # Create Transaction
+      #
+      # {
+      #     "merchantKey": "xxxxxxxxxxxxxxxxx",
+      #     "amount": 7500,
+      #     "metaId": "random number, token or code",
+      #     "overwriteMetaId": false,
+      #     "paymentMethod": [
+      #       {
+      #         "cardNonce": "xxxxxxxxxxxxxxxxx",
+      #         "cardBrandId": 2,
+      #         "paymentMode": 1,
+      #         "installmentType": 1,
+      #         "installments": 1,
+      #         "softDescriptor": "Dr. Zeus Inc.",
+      #         "amount": 7500,
+      #         "forceSplit": false
+      #       }
+      #     ],
+      #     "customerInfo": {
+      #       "fullName": "Joao Silva",
+      #       "cpf": "02700790090",
+      #       "phoneNumber": "515818282",
+      #       "birthday": "1994-05-02",
+      #       "emailAddress": "joao@email.com",
+      #       "zipCode": "902848",
+      #       "address": "Rua exemplo 123",
+      #       "neighborhood": "Bairro examplo",
+      #       "city": "Cidade exemplo",
+      #       "state": "Estado exemplo"
+      #     },
+      #     "autoCapture": true,
+      #     "postbackURL": "www.example.com/postback/"
+      #   }
+      def create(body)
+        request(:post, endpoint_create, body: body)
       end
 
-      # TO-DO
-      # def capture(transaction_id)
+      # POST
+      # Capture Transaction
       #
-      # end
+      # {
+      #   "merchantKey": "0123456789ABCDEF01...",
+      #   "transactionId": "1843246811",
+      #   "metaId": null,
+      #   "amount": null
+      # }
+      def capture(body)
+        request(:post, endpoint_capture, body: body)
+      end
 
       private
 
-      def endpoint
+      def endpoint_create
         API_URL + '/createTransaction'
       end
 
-      def build_transaction_body(card_token)
-        {
-            "merchantKey": merchant_key,
-            "amount": 7500,
-            "installmentType": 0,
-            "installments": nil,
-            "metaId": "AF85GHQ97A",
-            "overwriteMetaId": false,
-            "paymentMethod": build_payment_method(card_token),
-            "customerInfo": build_customer_info,
-            "autoCapture": false,
-            "postbackURL": "www.example.com/postback/",
-            "metadata": nil
-        }
-      end
-
-      def build_payment_method(card_token)
-        {
-            "cardNonce":  card_token,
-            "cardBrandId": 1,
-            "cardToken": nil,
-            "paymentMode": 1,
-            "dueDate": nil,
-            "softDescriptor": "Dr. Zeus Inc.",
-            "forceSplit": false
-        }
-      end
-
-      def build_customer_info
-        {
-            "fullName": "Joao Silva",
-            "cpf": "091.544.560-37",
-            "cnpj": nil,
-            "phoneNumber": "515818282",
-            "birthday": "1994-05-02",
-            "emailAddress": "joao@email.com",
-            "zipCode": "902848",
-            "address": "Rua exemplo 123",
-            "neighborhood": "Bairro examplo",
-            "city": "Cidade exemplo",
-            "state": "Estado exemplo"
-        }
+      def endpoint_capture
+        API_URL + '/captureTransaction'
       end
     end
   end
